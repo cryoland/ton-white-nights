1c2f8d5f6d0d2aa85cf8442d32dc0dac94c983a9
ValenTiN Konov <vsrknv@gmail.com>
2024-06-02 08:04:45 +0300

    tma domain changed

--- frontend/tma/src/main.tsx
+++ frontend/tma/src/main.tsx
@@ -3,7 +3,7 @@ import { App } from './App';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 
 const root = createRoot(document.getElementById('app') as HTMLElement);
-const manifestUrl = "https://right-robin-great.ngrok-free.app/manifest.json";
+const manifestUrl = "https://ton-wn-tma.ngrok.dev/manifest.json";
 
 root.render(
   <TonConnectUIProvider manifestUrl={manifestUrl}>
